782e28aae216a845c1c95ba1a3bc2bd7cbdeae59
ManuDev03 <manuprajan03@gmail.com>
2021-07-22 17:20:49 +0530

    data binding

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angularApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularApp');
+    expect(app.name).toEqual('angularApp');
   });
 
   it('should render title', () => {
